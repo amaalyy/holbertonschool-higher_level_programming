e91639a5452289ccd8e41f9a3e41c0e249220b54
amaalyy <amal.elzowawi@gmail.com>
2023-09-26 22:38:57 +0100

    Play with strings

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,4 +1,5 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-print(f"welcom to {str1} {str2}!")
+str1, str2 = str2, str1
+print("Welcome to {} {}!".format(str1, str2))
