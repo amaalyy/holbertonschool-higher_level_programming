7d0ce5a099987206a162063e2e35bcbe6580ca41
amaalyy <amal.elzowawi@gmail.com>
2023-09-29 22:56:34 +0100

    fix error

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
-#!/usr/local/bin/pyhton3
+#!/usr/bin/python3
 if __name__ == "__main__":
     import sys
     result = 0
-    for x in range(len(sys.argv) - 1):
-        result += int(sys.argv[x + 1])
-        print("{}".format(result))
+    for i in range(len(sys.argv) - 1):
+        result += int(sys.argv[i + 1])
+    print("{}".format(result))
