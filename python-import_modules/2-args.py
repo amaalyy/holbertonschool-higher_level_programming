dd94f10d7b0f3eb733088c9b4b299fad044348ea
amaalyy <amal.elzowawi@gmail.com>
2023-09-29 22:10:55 +0100

    test

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    x = len(sys.argv) - 1
+    if x == 0:
+        print("0 argments.")
+    elif x == 1:
+        print("1 arguments")
+    else:
+        print("{}arguments:".format(x))
