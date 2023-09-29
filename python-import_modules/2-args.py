3288f7f250dbc19c397e5ec4ad382ddeda1007ed
amaalyy <amal.elzowawi@gmail.com>
2023-09-29 22:29:45 +0100

    fix error

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    x = len(sys.argv) - 1
-    if x == 0:
+    argc = len(sys.argv) - 1
+    if argc == 0:
         print("0 argments.")
-    elif x == 1:
+    elif argc == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(x))
-    for i in range(x):
+        print("{} arguments:".format(argc))
+    for i in range(argc):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
