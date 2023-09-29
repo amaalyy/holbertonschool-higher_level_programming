50e9e20a76ad3c9910f85c4428128408bc9bead7
amaalyy <amal.elzowawi@gmail.com>
2023-09-29 22:18:22 +0100

    fix error

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if x == 0:
         print("0 argments.")
     elif x == 1:
-        print("1 arguments")
+        print("1 argument")
     else:
         print("{} arguments:".format(x))
     for i in range(x):
