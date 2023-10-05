0cd3b8d31ae300acad432546d2abdae8f7938ba2
amaalyy <amal.elzowawi@gmail.com>
2023-10-05 13:09:43 +0100

    fix error

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,8 +2,10 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
+    
     max = my_list[0]
     for i in my_list:
         if i > max:
             max = i
+            
     return max
