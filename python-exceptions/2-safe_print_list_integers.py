5cb3f86a54155fb235a8d5b895b97e23eac952a6
amaalyy <amal.elzowawi@gmail.com>
2023-10-08 19:23:36 +0100

    fix error

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     try:
-        j = 0
-        for i in my_list:
-            if j < x and type(i) == int:
-                print("{:d}".format(i), end="")
-                j += 1
-    except BaseException:
-        raise IndentationError("list index out of range")
-    print(end="\n")
-    return j
+        count = 0
+        for i in range(x):
+            if (type(my_list[i]) == int):
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+        print()
+        return (count)
+    except IndexError:
+        raise IndexError("list index out of range")
+    
