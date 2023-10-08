09723cbe2630dc452a0c3891c7d456228c908927
amaalyy <amal.elzowawi@gmail.com>
2023-10-08 13:05:04 +0100

    fix error

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or not isinstance(roman_string, str):
-        return 0
-    roman_value = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000}
-    int_value = 0
+    if (not roman_string or not isinstance(roman_string, str)):
+        return (0)
+
+    roman_list = {'I': 1, 'V': 5, 'X': 10,
+                  'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    value = 0
     for i in range(len(roman_string)):
-        if i > 0 and roman_value[roman_string[i]
-                                 ] > roman_value[roman_string[i - 1]]:
-            int_value += roman_value[roman_string[i]] - \
-                2 * roman_value[roman_string[i - 1]]
+        if (i+1 != len(roman_string) and
+                roman_list[roman_string[i]] < roman_list[roman_string[i+1]]):
+            value -= roman_list[roman_string[i]]
         else:
-            int_value += roman_value[roman_string[i]]
-        return int_value
+            value += roman_list[roman_string[i]]
+
+    return (value)
