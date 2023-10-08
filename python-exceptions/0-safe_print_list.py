10fc4512c88d0e6c3ef7d059cd28da09759537aa
amaalyy <amal.elzowawi@gmail.com>
2023-10-08 17:38:42 +0100

    prints x elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        j = 0
+        for i in my_list:
+            if (j < x):
+                print("{}".format(i), end="")
+                j += 1
+    except BaseException:
+        return (0)
+    print(end="\n")
+    return (j)
