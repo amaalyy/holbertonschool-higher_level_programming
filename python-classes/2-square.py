afb4864a1d164df7e63be96af864ebdf60b3434d
amaalyy <amal.elzowawi@gmail.com>
2023-10-09 21:06:05 +0100

    Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """class square"""
+
+
 class Square:
     """"class square"""
-    def __init__(self, size = 0):
+
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an interger")
         if size < 0:
