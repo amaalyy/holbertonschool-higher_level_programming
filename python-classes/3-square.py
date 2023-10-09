1818a2a61e206568d79d740f790d8ef989f10a99
amaalyy <amal.elzowawi@gmail.com>
2023-10-09 22:51:37 +0100

    fix pycodestyle

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -13,6 +13,6 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self._Square__size = size
+
     def area(self):
         return self._Square__size**2
-    
