5ebac179514e2c0c420cc44ff43d5bf734ed46a6
amaalyy <amal.elzowawi@gmail.com>
2023-10-09 21:15:02 +0100

    fix error

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,8 +6,8 @@ class Square:
     """"class square"""
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        if type(size) != int:
             raise TypeError("size must be an interger")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must me >=0")
         self.__size = size
