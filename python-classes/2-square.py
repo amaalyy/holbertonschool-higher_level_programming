923527aef73ff0fc7f3bd2a1c390c95b29f3daa5
amaalyy <amal.elzowawi@gmail.com>
2023-10-09 22:47:44 +0100

    fix error

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,10 +7,9 @@ class Square:
     """__init__ method"""
 
     def __init__(self, size=0):
-        if (type(size) != int):
+        if (not isinstance(size, int)):
             raise TypeError("size must be an integer")
         elif (size < 0):
             raise ValueError("size must be >= 0")
         else:
             self._Square__size = size
-        
