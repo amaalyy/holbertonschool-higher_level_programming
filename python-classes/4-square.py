397fdbf5b231b28ba6c26eb1ed1678b120aa10d5
amaalyy <amal.elzowawi@gmail.com>
2023-10-10 17:21:37 +0100

    fix pycodestyle

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """class of square"""
+
+
 class Square:
     """__init__ method"""
+
     def __init__(self, size=0):
         self.__size = size
     """property"""
@@ -11,12 +14,13 @@ class Square:
     """setter method"""
     @size.setter
     def size(self, value):
-        if (type(value) != int):
+        if (not isinstance(value, int)):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
     """area method"""
-    def area (self):
+
+    def area(self):
         return self.__size**2
