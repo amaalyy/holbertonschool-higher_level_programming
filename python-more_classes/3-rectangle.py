9e42aa3305fc6693ca1b9fb3f4283a1fadf00727
amaalyy <amal.elzowawi@gmail.com>
2023-10-12 15:22:59 +0100

    fix error

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
     """set the width"""
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -28,31 +28,29 @@ class Rectangle:
     """set the height"""
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
     """calculate the retangle area"""
-
     def area(self):
-        return self.__width * self.height
+        return self.__width*self.height
     """calculate the retangle perimeter"""
-
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.height)
-
+        return 2*(self.__width+self.height)
     def __str__(self):
         if self.width == 0 or self.height == 0:
-            return ("")
+            return("")
         else:
             x = ""
             for i in range(self.height):
                 for j in range(self.width):
                     x += "#"
-                if self.height - 1 != i:
+                if self.height-1 != i:
                     x += '\n'
             return x
+            
