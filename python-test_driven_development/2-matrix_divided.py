2f601cfc5f6984e6638c3977a9047471a8e5a584
amaalyy <amal.elzowawi@gmail.com>
2023-10-12 22:48:33 +0100

    fix error

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """devide matrix"""
+
+
 def matrix_divided(matrix, div):
     new_mat = []
-    if type(div) != int and type(div) != float:
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
@@ -11,13 +13,18 @@ def matrix_divided(matrix, div):
         c = len(matrix[0])
         if c != len(matrix[i]):
             raise TypeError("Each row of the matrix must have the same size")
-        if type(matrix[i]) != list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if not isinstance(matrix[i], list):
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
         for j in range(len(matrix[i])):
-            if type (matrix[i][j]) != int and type(matrix[i][j]) != float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            if not isinstance(
+                    matrix[i][j],
+                    int) and not isinstance(
+                    matrix[i][j],
+                    float):
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats")
             else:
-                new_mat1.append(round (matrix[i][j] / div, 2))
+                new_mat1.append(round(matrix[i][j] / div, 2))
         new_mat.append(new_mat1)
     return new_mat
-        
