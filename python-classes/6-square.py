712bfad6b2782e161e3d7b821b451bbd44b89fb0
amaalyy <amal.elzowawi@gmail.com>
2023-10-13 19:39:58 +0100

    fix error

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,8 +7,8 @@ class Square:
     """__init__ method"""
 
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     """property"""
     @property
