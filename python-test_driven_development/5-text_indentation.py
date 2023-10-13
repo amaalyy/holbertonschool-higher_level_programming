fefda0ec131c11c800eb1ac1c6ff552d3d61c36f
amaalyy <amal.elzowawi@gmail.com>
2023-10-13 13:33:58 +0100

    Text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    for i in range(len(text)):
+        if text[i] == '.' or text[i] == "?" or text[i] == ":":
+            print()
+            print()
+        else:
+            print(text[i], end="")
