84a4229acd1628ae74d9b7e5485cfee002d3c8c7
amaalyy <amal.elzowawi@gmail.com>
2023-10-13 13:40:22 +0100

    fix

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -2,6 +2,7 @@
 def text_indentation(text):
     for i in range(len(text)):
         if text[i] == '.' or text[i] == "?" or text[i] == ":":
+            print(text[i])
             print()
             print()
         else:
