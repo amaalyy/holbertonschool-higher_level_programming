181d39fb3211f7cbedbd1d22f467968fd7308271
amaalyy <amal.elzowawi@gmail.com>
2023-10-13 13:15:11 +0100

    fix pycodestyle

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,7 +4,7 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    for i in range(1, size * size +1):
+    for i in range(1, size * size + 1):
         if i % size == 0:
             print('#')
         else:
