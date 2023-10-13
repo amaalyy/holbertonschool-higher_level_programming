aebf37d6dc0a7508e153d58aaca4a835575d8283
amaalyy <amal.elzowawi@gmail.com>
2023-10-13 13:11:28 +0100

    Print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_square(size):
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(1, size * size +1):
+        if i % size == 0:
+            print('#')
+        else:
+            print("#", end="")
