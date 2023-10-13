764d96e1089a7e92779f9ed11a93675762dbfd75
amaalyy <amal.elzowawi@gmail.com>
2023-10-13 14:06:02 +0100

    fix

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -2,10 +2,7 @@
 def text_indentation(text):
     if type(test) != str:
         raise TypeError("text must be a string")
-    for i in range(len(text)):
-        if text[i] == "." or text[i] == "?" or text[i] == ":":
-            print(text[i])
-            print()
-            print()
-        else:
-            print(text[i], end="")
+    chars = [".", "?", ":"]
+    for char in chars:
+        text = text.replace(char, char + "\n\n")
+    print(text.strip(), end= "")
