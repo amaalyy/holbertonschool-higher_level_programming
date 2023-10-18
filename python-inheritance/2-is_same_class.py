ed05baff18584ec950a641145c229b5a6e1df10b
amaalyy <amal.elzowawi@gmail.com>
2023-10-18 21:21:30 +0100

    fix error

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     """Exact same object"""
-    if type(obj) != a_class:
-        return False
-    else:
+    if type(obj) is a_class:
         return True
+    else:
+        return False
