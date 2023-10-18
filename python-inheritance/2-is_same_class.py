7034c6e0d18789c3a79fccfb720feb40a368998a
amaalyy <amal.elzowawi@gmail.com>
2023-10-18 21:25:52 +0100

    fix error

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""return True if the object is exactly an instance of the specified class otherwise False"""
+"""return True if the object is exactly an instance of 
+the specified class otherwise False"""
 
 
 def is_same_class(obj, a_class):
