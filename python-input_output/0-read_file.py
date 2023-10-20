29a233ebea815031b8f13e813b1c346e91a8b072
amaalyy <amal.elzowawi@gmail.com>
2023-10-20 19:09:53 +0100

    test

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    f = open(filename, 'r', encoding="UTF8")
+    for c in f:
+        print(c)
