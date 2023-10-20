a3af519d1b4e935a5451a345f1fe7b6ccc85138b
amaalyy <amal.elzowawi@gmail.com>
2023-10-20 21:43:02 +0100

    fix error

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """function that creates an Object from a “JSON file”"""
 import json
+
+
 def load_from_json_file(filename):
     """Create object from a JSON file"""
-    return (json.load(filename))
+    with open(filename, 'r') as file:
+        return (json.load(file))
