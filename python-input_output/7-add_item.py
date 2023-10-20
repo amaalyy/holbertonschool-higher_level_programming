dc75ca142d43867ae655d171f7fcedcf4e2a52a7
amaalyy <amal.elzowawi@gmail.com>
2023-10-20 22:24:51 +0100

    Load, add, save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" script that adds all arguments to a Python list, and then save them to a file"""
+import sys
+import os
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+file_name = "add_item.json"
+with open(file_name, 'a') as file:
+    """if file is not empty"""
+    if (os.path.getsize(file_name) != 0):
+        items = load_from_json_file(file_name)
+        items += sys.argv[1:]
+        save_to_json_file(items, file_name)
+        """if file is empty"""
+    else:
+        items = []
+        items += sys.argv[1:]
+        save_to_json_file(items, file_name)
+        
