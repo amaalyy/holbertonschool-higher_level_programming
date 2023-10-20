a1dd38b0bd4209939dfd8e5420660483c38d0881
amaalyy <amal.elzowawi@gmail.com>
2023-10-20 23:16:38 +0100

    Student to JSON with filter

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -13,9 +13,8 @@ class Student:
     """retrives a dictionary"""
 
     def to_json(self, attrs=None):
-        if (attrs):
-            return {
-                key: value for key,
-                value in self.__dict__.items() if key in attrs}
-        else:
+        if (attrs is None):
             return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+                       
