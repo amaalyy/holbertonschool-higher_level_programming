b828943c167a25044e83039e87f83fd85a6190f5
amaalyy <amal.elzowawi@gmail.com>
2023-10-20 19:14:58 +0100

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Read file"""
-    with open(filename, 'r', encoding="UTF8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         print(f.read())
