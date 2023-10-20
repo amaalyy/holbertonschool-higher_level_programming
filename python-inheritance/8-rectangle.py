d8ef8cb1fc20180adb845faae972bae797919b53
amaalyy <amal.elzowawi@gmail.com>
2023-10-20 17:42:20 +0100

    Rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Improve Geometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+    """Integer validator"""
+
+    def integer_validator(self, name, value):
+        self.name = name
+        if type(value) != int:
+            raise TypeError(f"{self.name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{self.name} must be greater than 0")
+        else:
+            self.value = value
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = self.integer_validator("width", width)
+        self.height = self.integer_validator("height", height)
