5b955ddb81163b58f6c49bf631af36438435d820
amaalyy <amal.elzowawi@gmail.com>
2023-10-20 23:47:39 +0100

    fix error

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""function def pascal_triangle(n): that returns a 
+"""function def pascal_triangle(n): that returns a
 list of lists of integers representing the triangle of n"""
+
+
 def pascal_triangle(n):
     """Pascal's Triangle """
     res = []
@@ -12,7 +14,7 @@ def pascal_triangle(n):
         else:
             row = [1]
             for j in range(1, i):
-                row.append(res[i-1][j-1] + res[i-1][j])
+                row.append(res[i - 1][j - 1] + res[i - 1][j])
             row.append(1)
             res.append(row)
     return res
