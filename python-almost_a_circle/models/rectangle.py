97173ed5b153e6291d60a05475b73126e38bcf89
amaalyy <amal.elzowawi@gmail.com>
2023-10-26 20:32:19 +0100

    First Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""import class base"""
+from models.base import Base
+"""class Rectangle"""
+
+
+class Rectangle(Base):
+    """__init__ method"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__heigh = height
+        self.__x = x
+        self.__y = y
+    # width property
+
+        @property
+        def width(self):
+            return self.__width
+
+        @width.setter
+        def width(self, width):
+            if (type(width) == int):
+                self.__width = width
+    # height property
+
+        @property
+        def height(self):
+            return self.__height
+
+        @height.setter
+        def height(self, height):
+            if (type(height) == int):
+                self.__height = height
+    # x property
+
+        @property
+        def x(self):
+            return self.__x
+
+        @x.setter
+        def x(self, x):
+            if (type(x) == int):
+                self.__x = x
+    # y property
+
+        @property
+        def y(self):
+            return self.__y
+
+        @y.setter
+        def y(self, y):
+            if (type(y) == int):
+                self.__y = y
