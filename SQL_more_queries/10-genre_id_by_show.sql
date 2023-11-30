30241165baa045b760fd76c3732d7b6f27219f0a
amaalyy <amal.elzowawi@gmail.com>
2023-11-30 23:22:38 +0100

    Genre ID by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 SELECT tv_show_title, tv_show_genres.genre_id
 FROM tv_shows INNER JOIN tv_show_genres
 ON tv_show.id = tv_show_genres.show_id
-ORDER BY tv_show_title AND tv_show_genres.genre_id ASC;
+ORDER BY tv_show_title, tv_show_genres.genre_id ASC;
