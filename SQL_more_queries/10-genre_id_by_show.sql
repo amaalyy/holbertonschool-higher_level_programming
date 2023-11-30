d1d236130d81fac7f7e715a79ca9ee9bd70c0c91
amaalyy <amal.elzowawi@gmail.com>
2023-11-30 23:21:52 +0100

    Genre ID by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- script that lists all shows contained in hbtn_0d_tvshows
 SELECT tv_show_title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN tv_show_genres
-ON tv_show.id = tv_show_genres.genre_id
-ORDER BY tv_show_title, tv_show_genres.genre_id ASC;
+FROM tv_shows INNER JOIN tv_show_genres
+ON tv_show.id = tv_show_genres.show_id
+ORDER BY tv_show_title AND tv_show_genres.genre_id ASC;
