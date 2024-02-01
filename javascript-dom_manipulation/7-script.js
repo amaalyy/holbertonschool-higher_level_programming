33fa9db1188238ca89554d76ce2ed34081d3432e
amaalyy <amal.elzowawi@gmail.com>
2024-02-01 01:02:03 +0100

    Star Wars movies

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,20 @@
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/?format=json';
+const listMoviesElement = document.getElementById('list_movies');
+
+async function fetchAndListMovies() {
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+    const data = await response.json();
+
+    data.results.forEach((movie) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = movie.title;
+      listMoviesElement.appendChild(listItem);
+    });
+  } catch (error) {
+    console.error('Fetch error:', error);
+  }
+}
+
+fetchAndListMovies();
